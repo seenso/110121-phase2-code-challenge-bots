5a6f265dfda34af8e93f5cdf4acea11928678c8d
seenso <>
2021-12-03 08:09:22 -0900

    rendering all bots in BotCollection component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BotsPage from "./BotsPage";
 
+const api_url = "http://localhost:8002/bots";
+
 function App() {
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch(api_url)
+    .then(r => r.json())
+    .then(data => setBots(data))
+  }, []);
+
   return (
     <div className="App">
-      <BotsPage />
+      <BotsPage bots={bots}/>
     </div>
   );
 }
--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,15 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({ bots }) {
   // Your code here
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
-        Collection of all bots
+        {bots.map(b => (
+          <BotCard bot={b}/>
+        ))}
       </div>
     </div>
   );
